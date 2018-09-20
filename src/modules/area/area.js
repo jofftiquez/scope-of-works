0963ea466a846cb0c9fb8c73072e84d7ba2d9b5e
Jofferson R Tiquez <jofftiquez@gmail.com>
2018-09-20 13:03:48 +0800

    refactor(moved files):

--- src/modules/area/area.js
+++
@@ -1,6 +0,0 @@
-export const area = (length, width) => {
-  if(typeof length !== 'number' || typeof width !== 'number') {
-    return null;
-  }
-  return length * width
-}; 
--- src/modules/volume/circle.js
+++
@@ -1,9 +0,0 @@
-export const circleVol = (diameter, length) => {
-
-  if(typeof diameter !== 'number' || typeof length !== 'number') {
-    return null;
-  }
-
-  return 3.14 * (Math.pow(diameter, 2)) * 0.25 * length;
-  
-}
