4e60cf29021d4958b8be3c1c0c77b2437c524b5e
Jofferson R Tiquez <jofftiquez@gmail.com>
2018-10-04 00:41:04 +0800

    Added return in comment

--- src/modules/area/rectangle/rectangle.js
+++ src/modules/area/rectangle/rectangle.js
@@ -2,8 +2,8 @@
  * Calculate the Area of a Rectangle
  * @param {Number} length Distance for side A
  * @param {Number} width  Distance for side B
+ * @return Area of rectangle
  */
-
 export const areaRectangle = (length, width) => {
   if(typeof length !== 'number' || typeof width !== 'number') {
     return null;
--- src/modules/area/square/square.js
+++ src/modules/area/square/square.js
@@ -1,8 +1,8 @@
 /**
  * Calculate the Area of a Square
  * @param {Number} length Length of a square
+ * @return Area of square
  */
-
 export const areaSquare = (length) => {
   if(typeof length !== 'number') {
     return null;
