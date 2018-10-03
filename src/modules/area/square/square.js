591ca1069a44fe030556df0aa3ca6b4be4784bc8
Jensen96 <a.jensen1996@gmail.com>
2018-10-03 17:05:40 +0200

    corrected mistake in comment

--- src/modules/area/square/square.js
+++ src/modules/area/square/square.js
@@ -1,4 +1,4 @@
-// returns length multiplied with width, unless either of them doesen't equal 'number'
+// returns length multiplied with width, unless either of them doesen't equal the same type as 'number'
 export const areaSquare = (length, width) => {
   if(typeof length !== 'number' || typeof width !== 'number') {
     return null;
