5a2492431091238c716e267ee36207ffccda035e
Jannik <jannik@outlook.com>
2018-10-03 16:50:43 +0200

    Area docs

--- src/modules/area/trapezoid/trapezoid.js
+++ src/modules/area/trapezoid/trapezoid.js
@@ -1,3 +1,17 @@
+/**
+ * Calculate the Area of a Trapezoid.
+ * ```
+ * a  +--+     +
+ *   +    +    | h
+ *  +      +   |
+ * +--------+  +
+ * b
+ * ```
+
+ * @param {Number} a See ASCII art above.
+ * @param {Number} b See ASCII art above.
+ * @param {Number} h See ASCII art above.
+ */
 export const areaTrapezoid = (a, b, h) => {
   if(typeof a !== 'number' || typeof b !== 'number' || typeof h !== 'number') {
     return null;
